5a8207044bccb8b9ee97ad078be530edf9326c9b
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-03-25 09:38:55 -0400

    adjusting the number of properties displayed on property listings page to fix 3 per row

--- src/app/properties/properties.ts
+++ src/app/properties/properties.ts
@@ -1,33 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { PropertyService } from '../property';
-import { PropertyModel } from '../models/property';   // ← new clean import
+import { PropertyModel } from '../models/property';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-properties',
-  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './properties.html',
   styleUrl: './properties.scss'
 })
-export class PropertyComponent implements OnInit {
+export class PropertyComponent {
   properties$: Observable<PropertyModel[]>;
 
-  constructor(private propertyService: PropertyService) {
+  constructor(private readonly propertyService: PropertyService) {
     this.properties$ = this.propertyService.getProperties();
-  }
-
-  ngOnInit(): void {
-    this.propertyService.getProperties().subscribe(data => {
-      this.properties$ = this.propertyService.getProperties();
-    });
   }
 
   viewDetails(id: number): void {
     console.log('View details for property ID:', id);
-    // TODO: Open modal or navigate to /properties/:id
   }
 }
