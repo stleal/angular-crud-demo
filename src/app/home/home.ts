e62d37fcceb58bdbfb4195c3addec5f20ee33cc4
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-03-24 21:33:24 -0400

    adding intrinsic layout and stylesheet to the spa

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {}
